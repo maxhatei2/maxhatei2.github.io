fb43d45ecf3f47fd148164e14d188380703cbbfb
maxhatei2 <93264073+maxhatei2@users.noreply.github.com>
2024-01-02 19:05:22 +0200

    Added things
    What's new:
    
    - No more random "text" button
    
    - New scrollbar (works on Windows 10)
    
    - Added "cement mode" (cement texture background + the image is now rotating on hover

---
+++ script.js
@@ -0,0 +1,11 @@
+function LightMode() {
+	document.body.style.background = '#FFFFFF';
+}
+
+function CemMode() {
+	document.body.style.background = '#707070';
+}
+
+function ColorBg(color) {
+	document.body.style.background = color;
+}
